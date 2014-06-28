147da32fa40dac335a7d301a77a8fc8eec306f25
root <root@six.(none)>
2014-06-28 10:00:01 +1000

    Auto update from Six

---
+++ Accel_Distance_1/NoiseDeadBandFilter.cpp
@@ -0,0 +1,70 @@
+/*
+* Noise Dead Band Filter Class
+*
+* Library for a simple Noise Dead Band filter
+*
+* Requirements: -
+*
+* TODO: - 
+*
+* REF: - 
+*
+* USAGE:
+*   NoiseDeadBandFilter NDBF;            // create/declare with default parameters
+*
+*   NDBF.SetRange(double Upper, double Lower);  //Set the range of the Dead Band
+*
+*   while (true)
+*   {
+*       double value = getValueFromSensor();
+*       double correctedValue = NDBF.Filter(double value);
+*   }
+*/
+
+#ifndef NOISE_DEAD_BAND_FILTER_CLASS
+#define NOISE_DEAD_BAND_FILTER_CLASS
+
+class NoiseDeadBandFilter {
+public:
+    NoiseDeadBandFilter() {}
+     
+    double Filter(double data) {
+      LV = CV;
+
+      if(data > RU) { 
+        // slide the dead band range up
+        RL = (data - RU) + RL;
+        RU = data;
+      }
+
+      if(data < RL) { 
+        // slide the dead band range down
+        RU = RU - (RL - data);
+        RL = data; 
+      }
+      
+      CV = (RU - RL) / 2 + RL;
+
+      return CV;
+    }
+
+    void SetRange(double u, double l) {
+      RU = u;
+      RL = l;
+    }
+
+    double GetRange() { return (RU - RL); }
+
+    double GetRU() { return RU; }
+
+    double GetRL() { return RL; }
+
+private:
+    double RU; // Range Upper
+    double RL; // Range Lower
+
+    double CV; // Current Value
+    double LV; // Last Value
+};
+
+#endif    // NOISE_DEAD_BAND_FILTER_CLASS
