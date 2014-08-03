0b7e61e0e4da5af8a08abf3feb0e9808b9034ac9
root <root@six.(none)>
2014-08-03 13:00:02 +1000

    Auto update from Six

--- ADXL345_Example_Code_1/ADXL345_Example_Code_1.ino
+++ ADXL345_Example_Code_1/ADXL345_Example_Code_1.ino
@@ -20,20 +20,26 @@ void setup()
 void loop() 
 { 
   int regAddress = 0x32;    //first axis-acceleration-data register on the ADXL345 
-  int x, y, z; 
+  int16_t x, y, z; 
   
   readFrom(DEVICE, regAddress, TO_READ, buff); //read the acceleration data from the ADXL345 
   
   //each axis reading comes in 10 bit resolution, ie 2 bytes.  Least Significat Byte first!! 
   //thus we are converting both bytes in to one int 
-  x = (((int)buff[1]) << 8) | buff[0];    
-  y = (((int)buff[3])<< 8) | buff[2]; 
-  z = (((int)buff[5]) << 8) | buff[4]; 
+  x = (buff[1] << 8) | buff[0];    
+  
+  Serial.print("buff0 = ");
+  Serial.print(buff[0]);
+  Serial.print("\tbuff1 = ");
+  Serial.println(buff[1]);
+  
+  y = (((int16_t)buff[3])<< 8) | buff[2]; 
+  z = (((int16_t)buff[5]) << 8) | buff[4]; 
   
   //we send the x y z values as a string to the serial port 
   sprintf(str, "%d %d %d", x, y, z);   
-  Serial.print(str); 
-  Serial.print(10, BYTE); 
+  Serial.println(str); 
+  //Serial.print(10, BYTE); 
   
   //It appears that delay is needed in order not to clog the port 
   delay(50); 
